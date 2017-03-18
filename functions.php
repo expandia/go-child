84b9e1524e88050ae3b2666937222efc8a671543
samueldsquire <sam_squire@msn.com>
2017-03-18 23:19:25 +0000

    Correct style and scripts link
    Signed-off-by: samueldsquire <sam_squire@msn.com>

--- functions.php
+++ functions.php
@@ -10,14 +10,14 @@ if( function_exists('acf_add_options_page') ) {
 add_action( 'wp_enqueue_scripts', 'expandia_enqueue_scripts' );
 
 function expandia_enqueue_scripts() {
-  wp_register_script( 'main', get_template_directory_uri() . '/js/main-min.js', array('jquery'), '1', false );
+  wp_register_script( 'main', get_stylesheet_directory_uri() . '/js/main-min.js', array('jquery'), '1', false );
   wp_enqueue_script( 'main' );
 
 }
 
 // Register Style
 function custom_styles() {
-	wp_register_style( 'styles', get_template_directory_uri() . '/style.css', false, false, 'all' );
+	wp_register_style( 'styles', get_stylesheet_directory_uri() . '/style.css', false, false, 'all' );
 	wp_enqueue_style( 'styles' );
 }
 add_action( 'wp_enqueue_scripts', 'custom_styles' );
